fc10c09d471b2b7c0ae5051bba780eb9c011e01f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-25 14:54:35 -0800

    separate car-dealership logic

---
+++ Car.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+  public string Message;
+
+  public Car(string makeModel, int price, int miles, string message)
+  {
+    MakeModel = makeModel;
+    Price = price;
+    Miles = miles;
+    Message = message;
+  }
+
+  public bool WorthBuyingPrice(int maxPrice, int maxMiles)
+  {
+    return (Price <maxPrice && Miles<maxMiles);
+  }
+
+  // public bool WorthBuyingMiles(int maxMiles)
+  // {
+  //   return (Miles < maxMiles);
+  // }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Dealership
+{
+
+  public class Program
+  {
+    public static void Main()
+    {
+      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, "Still the best car ever!");
+      Car yugo = new Car("1980 Yugo Koral", 700, 56000, "They don't make them like this anymore. Luckily.");
+      Car ford = new Car("1988 Ford Country Squire", 1400, 239001, "Boring, but still drives.");
+      Car amc = new Car("1976 AMC Pacer", 400, 198000, "Not sure what this is, but if you're into it...");
+
+      List<Car> Cars = new List<Car>() {volkswagen, yugo, ford, amc};
+
+      Console.WriteLine("Enter maximum price: ");
+      string stringMaxPrice = Console.ReadLine();
+      int maxPrice = int.Parse(stringMaxPrice);
+      Console.WriteLine("Enter maximum mileage: ");
+      string stringMaxMiles = Console.ReadLine();
+      int maxMiles = int.Parse(stringMaxMiles);
+
+      List<Car> CarsMatchingSearch = new List<Car>(0);
+
+      foreach(Car automobile in Cars)
+      {
+        if(automobile.WorthBuyingPrice(maxPrice, maxMiles))
+        {
+          CarsMatchingSearch.Add(automobile);
+        }
+      }
+      if(CarsMatchingSearch.Count > 0)
+      {
+        foreach(Car automobile in CarsMatchingSearch)
+        {
+          Console.WriteLine(automobile.MakeModel);
+          Console.WriteLine(automobile.Message);
+        }
+      }
+      else
+      {
+      Console.WriteLine("No cars for you!");
+      }
+    }
+  }
+}  
